5d894b6919c91948f5ac4fb5b080d9c992dad975
Parama Wattanakrai <Parama.Wattanakrai@gmail.com>
2025-03-22 01:20:52 +0700

    file organization

--- Main.java
+++ Main.java
@@ -3,29 +3,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "เครื่องรางของขลังวัดโบสถ์พันธุ์แท้";
+        String text = "สิทธิ์ลุ้นเครื่องรางของขลังวัดโบสถ์พันธุ์แท้";
         // String text = "หอมรดกไทย";
         // String text = "เรือน้อยลอยอยู่";
         // String text = "ประกันภัยสัมพันธ์";
         // String text = "ตากลม";
-        String splitText = "";
-        TCC fsm = new TCC();
-        for (int i = 0; i < text.length(); i++) {
-            fsm.transition(text.charAt(i));
-            if (fsm.getCurrentState() == States.END) {
-                splitText += text.charAt(i);
-                splitText += " ";
-                fsm = new TCC();
-                continue;
-            }
-            if (fsm.getCurrentState() == States.ENDBEFORE) {
-                splitText += " ";
-                fsm = new TCC();
-                i--;
-                continue;
-            }
-            splitText += text.charAt(i);
-        }
+
+        String splitText = TCC.split(text);
 
         try (FileWriter writer = new FileWriter("output.txt")) {
             writer.write("Split Text: " + splitText);
--- TCC.java
+++ TCC.java
@@ -1,46 +1,23 @@
-import java.util.List;
-
 public class TCC {
-    private static final List<Character> CONSONANTS = List.of(
-        'ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง',
-        'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ',
-        'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ',
-        'ด', 'ต', 'ถ', 'ท', 'ธ', 'น',
-        'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม',
-        'ย', 'ร', 'ล', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ'
-    );
-    private static final List<Character> FRONT_VOWELS = List.of(
-        'เ', 'แ', 'โ', 'ไ', 'ใ'
-    );
-    private static final List<Character> AFTER_REQUIRED = List.of(
-        'ั', '็',
-        '่', '้', '๊', '๋'
-    );
-    private static final List<Character> ENDING_CHARS = List.of(
-        'ะ', '์', ' ', 'ๆ', 'ฯ'
-    );
-
-    private States currentState;
-    
-    public TCC() {
-        currentState = States.START;
-    }
-
-    public States getCurrentState() {
-        return currentState;
-    }
-
-    public void transition(char input) {
-        switch (currentState) {
-            case START:
-                if (CONSONANTS.contains(input)) currentState = States.CONSONANT;
-                break;
-            case CONSONANT:
-                if (AFTER_REQUIRED.contains(input)) currentState = States.START;
-                if (FRONT_VOWELS.contains(input) || CONSONANTS.contains(input)) currentState = States.ENDBEFORE;
-                if (ENDING_CHARS.contains(input)) currentState = States.END;
-            default:
-                break;
+    public static String split(String text) {
+    String splitText = "";
+    FSM fsm = new FSM();
+    for (int i = 0; i < text.length(); i++) {
+        fsm.transition(text.charAt(i));
+        if (fsm.getCurrentState() == States.END) {
+            splitText += text.charAt(i);
+            splitText += " ";
+            fsm = new FSM();
+            continue;
         }
+        if (fsm.getCurrentState() == States.ENDBEFORE) {
+            splitText += " ";
+            fsm = new FSM();
+            i--;
+            continue;
+        }
+        splitText += text.charAt(i);
+    }
+    return splitText;
     }
 }
